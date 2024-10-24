8ce255e33d8236e7cd26fb6ca34bca30a9e805e5
henriquemantovan <henrique.mantovan@usp.br>
2024-10-24 14:34:50 -0300

    primeiro commit

---
+++ private-bills/app/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const LoginPage: React.FC = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    setError('');
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      setError('Por favor, preencha todos os campos.');
+      return;
+    }
+
+    console.log('Autenticando:', { email, password });
+    router.push('/dashboard');
+  };
+
+  const goToTarefas = () => {
+    router.push('/tarefas');
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h2 className="title">Página de Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Senha:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          {error && <p className="error">{error}</p>}
+          <button type="submit">Login</button>
+        </form>
+      </div>
+
+      <div style={{ position: 'fixed', bottom: '20px', left: '20px', zIndex: 1000 }}>
+        <button className="button" onClick={goToTarefas}       
+            style={{ padding: '30px 80px',fontSize: '30px',borderRadius: '5px' }}>
+          Ir para Tarefas (só de testes)
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ private-bills/app/tarefas/page.tsx
@@ -0,0 +1,11 @@
+
+const TarefasPage: React.FC = () => {
+    return (
+      <div>
+        <h2>Página de Tarefas</h2>
+      </div>
+    );
+  };
+  
+  export default TarefasPage;
+  
