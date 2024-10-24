4e04e7d23b3841ccaa47a83c30c00605a1fed3d5
claudiolcsilva <claudiolcsilva@outlook.com>
2024-10-24 15:53:47 -0300

    ideia de landing page

--- private-bills/app/tarefas/page.tsx
+++ private-bills/app/tarefas/page.tsx
@@ -6,28 +6,37 @@ import { useRouter } from 'next/navigation';
 const BillButtons: React.FC = () => {
   const router = useRouter();
 
-  const goToPagador = () => {
-    router.push('/pagador');
+  const goToPayMyBill = () => {
+    router.push('/PayMyBill');
   };
 
-  const goToComprador = () => {
-    router.push('/comprador');
+  const goToPayABill = () => {
+    router.push('/PayABill');
   };
 
   return (
-    <div className="flex justify-center items-center h-screen gap-x-8">
-      <button
-        className="button"
-        onClick={goToPagador}
+    <div className="flex flex-col bg-teal-500 justify-center items-center h-screen w-screen gap-x-8">
+
+      <h1 className="text-6xl font-serif font-bold text-white my-2">Sorry, was that your bill?</h1>
+      <h2 className="text-xl font-serif italic text-white mb-4">Using Lightning to provide a private bill payment solution</h2>
+      <h3 className="text-xl font-serif font-semibold text-white mb-4">Select an option:</h3>
+
+      <div className="flex justify-center gap-8">
+      <button  
+        className="bg-[#FFFAA0] h-[15vh] w-[30vw] hover:bg-[#FADA5E] text-gray-700 font-mono font-bold py-2 px-4 rounded-lg border-2 border-black"
+        onClick={goToPayMyBill}
       >
-        pay my bill, please
+        Pay my bill, please
       </button>
+
       <button
-        className="button"
-        onClick={goToComprador}
+        className="bg-[#FFFAA0] h-[15vh] w-[30vw] hover:bg-[#FADA5E] text-gray-700 font-mono font-bold py-2 px-4 rounded-lg border-2 border-black"
+        onClick={goToPayABill}
       >
-        that's on me!
+        That's on me!
       </button>
+
+      </div>
     </div>
   );
 };
