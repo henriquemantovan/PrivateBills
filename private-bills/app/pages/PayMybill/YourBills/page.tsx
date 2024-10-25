4d96f9e5e7ee773e4318cdca70a2f27fb5ddb42e
claudiolcsilva <claudiolcsilva@outlook.com>
2024-10-25 13:05:40 -0300

    upload de imagem da conta

--- private-bills/app/pages/PayMybill/YourBills/page.tsx
+++ private-bills/app/pages/PayMybill/YourBills/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import React, { useState } from 'react';
 import BillCard from '../components/BillCard';
 import Modal from '../components/BillModal';
--- private-bills/app/pages/PayMybill/page.tsx
+++ private-bills/app/pages/PayMybill/page.tsx
@@ -1,22 +1,41 @@
-"use client";
-
+'use client'
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 const PayMyBill = () => {
   const router = useRouter();
   const [formData, setFormData] = useState({ value: '', address: '' });
+  const [image, setImage] = useState<File | null>(null); // New state for image file
   const [showPopup, setShowPopup] = useState(false);
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    // Logic to handle submission can be added here (API call, form validation, etc.)
-    
-    // Show success popup
-    setShowPopup(true);
-    
-    // Clear the form after submission
-    setFormData({ value: '', address: '' });
+
+    // Create FormData object to handle both form data and image
+    const data = new FormData();
+    data.append('value', formData.value);
+    data.append('address', formData.address);
+    if (image) data.append('image', image);
+
+    try {
+      await axios.post('/api/paybill', data, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      setShowPopup(true);
+      setFormData({ value: '', address: '' });
+      setImage(null); // Clear image after submission
+    } catch (error) {
+      console.error('Error submitting form', error);
+    }
   };
 
   const goToYourBills = () => {
@@ -24,13 +43,12 @@ const PayMyBill = () => {
   };
 
   const goBack = () => {
-    router.back(); // Retorna à página anterior
+    router.back();
   };
 
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-teal-500">
       <div className="relative bg-[#FFFAA0] w-[40vw] p-6 rounded-lg border-2 border-black shadow-lg">
-        {/* "Your bills" button */}
         <div className="absolute top-4 right-4">
           <button
             onClick={goToYourBills}
@@ -39,11 +57,8 @@ const PayMyBill = () => {
             Your bills
           </button>
         </div>
-
-        {/* Title */}
         <h1 className="text-4xl font-serif font-bold text-gray-700 mb-6">Pay my bill</h1>
 
-        {/* Form */}
         <form onSubmit={handleSubmit} className="space-y-6">
           <div>
             <input
@@ -65,7 +80,16 @@ const PayMyBill = () => {
               required
             />
           </div>
-
+          <h1 className="text-md font-serif font-bold text-gray-700 mb-4">Submit an image from your bill (with a PIX copy and paste or barcode):</h1>
+          <div>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              className="w-full border-2 border-black rounded-md py-2 px-4 text-gray-700"
+              required
+            />
+          </div>
           <button
             type="submit"
             className="w-full h-[8vh] bg-[#FADA5E] hover:bg-[#FFD700] text-gray-700 font-mono font-bold py-2 px-4 rounded-lg border-2 border-gray"
@@ -74,7 +98,6 @@ const PayMyBill = () => {
           </button>
         </form>
 
-        {/* Success Popup */}
         {showPopup && (
           <div className="absolute inset-0 flex justify-center items-center bg-black bg-opacity-50">
             <div className="bg-white p-4 rounded-lg border-2 border-black shadow-lg">
